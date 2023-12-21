5de2675ff7c82bca1ca83e05d2688fcc802efa43
DavideTW <paolazzidavide67@gmail.com>
2023-12-21 15:17:23 +0100

    fix: login page? fix: jsonwebtoken

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,15 +3,32 @@ import { getAntdFieldRequiredRule } from '@/app/helpers/validation';
 import Button from 'antd/es/button';
 import Form from 'antd/es/form';
 import React from 'react'
+import {useRouter} from "next/router";
+import axios from "axios";
+import message from 'antd/es/message';
+
 interface userType {
     email: string;
     password: string;
 }
 
 function Login() {
-    const onLogin = (values: userType) => {
-        console.log(values);
-    }
+    const [loading, setLoading] = React.useState(false);
+    const router = useRouter();
+
+    const onLogin = async (values: userType) => {
+        try {
+            setLoading(true);
+            await axios.post("/api/auth/login", values);
+            message.success("Login successful");
+            router.push("/");
+        } catch (error: any) {
+            message.error(error.response.data.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div>
             <div className='grid grid-cols-2 min-h-screen'>
@@ -32,7 +49,7 @@ function Login() {
                         <Form.Item name="password" label="password" rules={getAntdFieldRequiredRule('Please input your password')}>
                             <input type='password' />
                         </Form.Item>
-                        <Button type='primary' htmlType='submit' block>
+                        <Button type='primary' htmlType='submit' block loading={loading}>
                             Login
                         </Button>
                     </Form>
