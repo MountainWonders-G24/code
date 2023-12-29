6781e31cb7d7606cb7f6be8a96cb63ded7f87277
Alessio Amiri <alessioamiri182@gmail.com>
2023-12-29 17:21:12 +0100

    Trying fetching all refuges

--- app/api/refuges/route.ts
+++ app/api/refuges/route.ts
@@ -5,8 +5,20 @@ import { connectDB } from "@/configs/dbConfig";
 connectDB();
 export async function GET() {
     try {
+        let refuges= new Array();
+        for (let i = 1; i < 4; i++) {
+            const result= ( await Refuge.find({
+                mountainId: i
+            }));
+            result.forEach(element => {
+                refuges.push(element);
+            });
+        }
+        console.log(refuges);
 
-        const refuges = await Refuge.find({});
+
+        
+        //const refuges = await Refuge.find({});
         return NextResponse.json({
             message: "Refuges retrieved!",
             data: refuges,
--- app/main-pages/[mountainName]/page.tsx
+++ app/main-pages/[mountainName]/page.tsx
@@ -207,9 +207,6 @@ function Refuges() {
         } else {
             displayAddButton(false);
             fetchRefuges('/api/refuges');
-            for (let i = 0; i < refuges.length; i++) {
-                console.log(refuges[i]);                
-            }
             (document.getElementById("mountain-name") as HTMLElement).innerHTML = "Rifugi del Trentino";
         }
 
