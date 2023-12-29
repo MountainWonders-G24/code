ebf17680f865425f3df9cf29a81aeed1ee06f66c
Alessio Amiri <alessioamiri182@gmail.com>
2023-12-30 00:53:32 +0100

    Added logout with abs path

--- app/script.tsx
+++ app/script.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 const response = () => {
-    console.log("logout");
-    axios.get('/api/auth/logout')};
+    console.log("logoutbgudsobgoa");
+    axios.get('@/app/api/auth/logout')};
 export async function logout(){
-    const currentUser = await axios.get('/api/auth/currentUser');
+    const currentUser = await axios.get('@/app/api/auth/currentUser');
     if (currentUser.data.status == 200) {
+        console.log("logged");
         (document.getElementById("logout") as HTMLElement).style.display = "block";
         (document.getElementById("logout") as HTMLElement).onclick = response;
     } else {
