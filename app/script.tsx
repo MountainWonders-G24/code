72f19542f2f4a1570483b32dce47733b03bd699b
Alessio Amiri <alessioamiri182@gmail.com>
2023-12-30 00:50:45 +0100

    Added logout

--- app/script.tsx
+++ app/script.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-const response = () => {axios.get('/api/auth/logout')};
+const response = () => {
+    console.log("logout");
+    axios.get('/api/auth/logout')};
 export async function logout(){
     const currentUser = await axios.get('/api/auth/currentUser');
     if (currentUser.data.status == 200) {
