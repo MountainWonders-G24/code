6e96e4838f82b22ea5dbed2fab1af19c627f1e9d
GioGio31 <gioevvai@gmail.com>
2023-12-30 10:25:09 +0100

    Logout as link

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export function GET(){
-    const response = NextResponse.json({
-        message: "Logout successful",
-        status: 200
-    });
+
+export async function GET()  {
     const cookieStore = cookies();
     console.log(cookieStore);
     cookieStore.delete('email');
     cookieStore.delete('token');
     //response.cookies.delete("token");
     //response.cookies.delete("email");
-    return response;
+    return NextResponse.json({
+        message: "Logout successful",
+        status: 200
+    });;
 }
--- app/script.tsx
+++ app/script.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
-const response = () => {
+const response = async () => {
     console.log("logoutbgudsobgoa");
-    const logout = axios.get('/api/auth/logout');
-    console.log("MEssaggio logout: " + logout)
+    const logout = await axios.get('/api/auth/logout');
+    console.log("MEssaggio logout: " + logout.data.message);
 };
 export async function logout() {
     const currentUser = await axios.get('/api/auth/currentUser');
