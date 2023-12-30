88e31015ee953df5b7aad3642bbe30eaf4505a0f
GioGio31 <gioevvai@gmail.com>
2023-12-30 10:00:26 +0100

    Changed cookies

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,12 +1,16 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export function GET(){
     const response = NextResponse.json({
         message: "Logout successful",
         status: 200
     });
-
-    response.cookies.delete("token");
-    response.cookies.delete("email");
+    const cookieStore = cookies();
+    console.log(cookieStore);
+    cookieStore.delete('email');
+    cookieStore.delete('token');
+    //response.cookies.delete("token");
+    //response.cookies.delete("email");
     return response;
 }
