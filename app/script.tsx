956ff324fe5bc015f9d7b9a9b4111ac5433b3bd2
GioGio31 <gioevvai@gmail.com>
2023-12-30 10:33:42 +0100

    Logout as link

--- app/script.tsx
+++ app/script.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 const response = async () => {
     console.log("logoutbgudsobgoa");
     const logout = await axios.get('/api/auth/logout');
-    console.log("MEssaggio logout: " + logout.data.message);
+    console.log("Messaggio logout: " + logout.data.message);
+    window.location.href = "/";
 };
 export async function logout() {
     const currentUser = await axios.get('/api/auth/currentUser');
@@ -10,6 +11,7 @@ export async function logout() {
         console.log("logged");
         (document.getElementById("logout") as HTMLElement).style.display = "block";
         (document.getElementById("logout") as HTMLElement).onclick = response;
+        
     } else {
         (document.getElementById("logout") as HTMLElement).style.display = "none";
     }
