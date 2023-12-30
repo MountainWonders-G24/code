9a3001ca83e81e26fcf151748a6328fa53688a87
Alessio Amiri <alessioamiri182@gmail.com>
2023-12-30 01:04:26 +0100

    TODO! fix logout button

--- app/script.tsx
+++ app/script.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 const response = () => {
     console.log("logoutbgudsobgoa");
-    axios.get('@/app/api/auth/logout')};
+    axios.get('/app/api/auth/logout')};
 export async function logout(){
-    const currentUser = await axios.get('@/app/api/auth/currentUser');
+    const currentUser = await axios.get('/app/api/auth/currentUser');
     if (currentUser.data.status == 200) {
         console.log("logged");
         (document.getElementById("logout") as HTMLElement).style.display = "block";
