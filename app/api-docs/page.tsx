050bb6fb25e1a2cad39e8103d057e31f0d8bd033
DavideTW <paolazzidavide67@gmail.com>
2024-01-01 14:17:05 +0100

    fixed swagger

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -1,7 +1,17 @@
 'use client'
-import SwaggerUI from "swagger-ui-react";
+import React from 'react';
 import "swagger-ui-react/swagger-ui.css"
+import swaggerFile from '../../swagger.json';
 
-export default function Home () {
-    return <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
-}
+import dynamic from "next/dynamic";
+
+const DynamicSwaggerUI = dynamic(() => import("swagger-ui-react"), {
+    ssr: false,
+    loading: () => <p>Loading Component...</p>,
+});
+
+const AdminDocPage = () => {
+    return <DynamicSwaggerUI spec={swaggerFile} />;
+};
+
+export default AdminDocPage;
