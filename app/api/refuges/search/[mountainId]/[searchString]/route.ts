4c780d68c2814fa8226ab64b7435a38061ea6d0e
Alessio Amiri <alessioamiri182@gmail.com>
2024-01-01 15:48:50 +0100

    Search fixed with different mountain params

--- app/api/refuges/search/[mountainId]/[searchString]/route.ts
+++ app/api/refuges/search/[mountainId]/[searchString]/route.ts
@@ -2,7 +2,7 @@
 import Refuge from "@/app/models/refugeModel";
 import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/configs/dbConfig";
-
+import axios from "axios";
 interface Params {
     mountainId: string;
     searchString: string;
@@ -16,12 +16,24 @@ export async function GET(request: NextRequest, { params }: { params: Params }) 
         console.log(params.mountainId);
         const refugeName = params.searchString;
         console.log("Refuge name: " + refugeName);
+        let refuges;
+        if (params.mountainId != "0") {
+            const mountain = await (axios.get('https://mountain-wonders.vercel.app/api/mountains/'+params.mountainId));
+            console.log(mountain);
+            if (mountain.data.status == 404) {
+                throw new Error("No mountain found")
+            }
+            refuges = await Refuge.find({
+                name: { $regex: refugeName, $options: "i" },
+                mountainId:  params.mountainId// "i" case-insensitive
+            });
+        }else{
+            refuges = await Refuge.find({
+                name: { $regex: refugeName, $options: "i" }
+            });
+        }
         
-
-        const refuges = await Refuge.find({
-            name: { $regex: refugeName, $options: "i" },
-            mountainId:  params.mountainId// "i" case-insensitive
-        });
+        
 
         // const refuges = await Refuge.find({
         //     name: reqBody.name
